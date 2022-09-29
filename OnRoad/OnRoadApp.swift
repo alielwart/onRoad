a794d98c8ec2834c94445967ef22f7afa15e9515
Allyson Elwart <74015789+alielwart@users.noreply.github.com>
2022-09-29 13:53:46 -0400

    Initial Commit

---
+++ OnRoad/OnRoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnRoadApp.swift
+//  OnRoad
+//
+//  Created by Ali Elwart on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct OnRoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
